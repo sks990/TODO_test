032ace38a6712939d85ea7642ec5dbb511a66273
sks990 <87841188+sks990@users.noreply.github.com>
2026-02-19 22:39:31 +0900

    [FEATURE] 공통 UI 구조 및 Todo/모달 컴포넌트 구현 - src/utils/constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export const VIEWS = {
+    TODO: 'todo',
+    KANBAN: 'kanban',
+    GANTT: 'gantt'
+};
+
+export const PRIORITIES = {
+    LOW: 'low',
+    MEDIUM: 'medium',
+    HIGH: 'high'
+};
+
+export const STATUSES = {
+    TODO: 'todo',
+    IN_PROGRESS: 'in_progress',
+    DONE: 'done'
+};
